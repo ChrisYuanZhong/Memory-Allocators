8033672f8b6879f755ea4ce8afe2ff73f6bfb2ca
ChrisYuanZhong <chrisyuanzhong@outlook.com>
2022-10-22 00:10:27 -0600

    1

--- Heap Allocator/Heap Allocator/HeapManager-eae-z490-121.h
+++ Heap Allocator/Heap Allocator/HeapManager-eae-z490-121.h
@@ -1,12 +1,5 @@
 #pragma once
 
-struct MemoryBlock
-{
-	void* pBaseAddress;
-	size_t BlockSize;
-	struct MemoryBlock* pNextBlock;
-};
-
 class HeapManager {
 public:
 	static HeapManager* create(void* i_pMemory, size_t i_sizeMemory, unsigned int i_numDescriptors);
@@ -15,17 +8,14 @@ public:
 	void* _alloc(size_t i_size, unsigned int i_alignment);
 	bool _free(void* i_ptr);
 	void collect();
-	bool Contains(void* i_ptr) const;
-	bool IsAllocated(void* i_ptr) const;
-	size_t getLargestFreeBlock() const;
-	size_t getTotalFreeMemory() const;
-	void ShowFreeBlocks() const;
-
-private:
-	struct MemoryBlock TheHeap;
-	struct MemoryBlock* FreeList = &TheHeap;
+	bool Contains(void* i_ptr);
+	bool IsAllocated(void* i_ptr);
+	size_t getLargestFreeBlock();
+	size_t getTotalFreeMemory();
+	void ShowFreeBlocks();
 };
 
 HeapManager* HeapManager::create(void* i_pMemory, size_t i_sizeMemory, unsigned int i_numDescriptors)
 {
+
 }
